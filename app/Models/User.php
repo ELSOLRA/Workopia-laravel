feb52b65cff2b2576373896ae14928e8f7142c84
ELSOLRA <bardakasmiljo@gmail.com>
2025-05-19 15:39:39 +0200

    migration and relationships for bookmarks

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -51,6 +52,11 @@ class User extends Authenticatable
     // Relate to Joblisting
     public function jobListings(): HasMany
     {
-        return $this->hasMany(Joblisting::class);
+        return $this->hasMany(Job::class); // Joblisting::class ???
+    }
+
+    public function bookmarkedJobs(): BelongsToMany
+    {
+        return $this->belongsToMany(Job::class, 'job_user_bookmarks')->withTimestamps();
     }
 }
