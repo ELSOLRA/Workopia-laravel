e4efb86d8397646cba02c837a2c502f437ad62ad
ELSOLRA <bardakasmiljo@gmail.com>
2025-05-20 21:51:51 +0200

    bookmarks auth route, view, controller

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,14 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\BookmarkController;
 
 /* use Illuminate\Http\Request; */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 // Route::resource('jobs', JobController::class);
-Route::resource('jobs', JobController::class)->middleware('auth')->only(['create', 'edit', 'update', 'destroy']);
+// Route::resource('jobs', JobController::class)->middleware('auth')->only(['create', 'edit', 'update', 'destroy']);
 
 Route::resource('jobs', JobController::class)->except(['create', 'edit', 'update', 'destroy']);
 
@@ -23,7 +24,13 @@ Route::middleware('guest')->group(function () {
     Route::get('/login', [LoginController::class, 'login'])->name('login');
     Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
 });
+Route::middleware('auth')->group(function () {
+    // Resource routes with restrictions
+    Route::resource('jobs', JobController::class)->only(['create', 'edit', 'update', 'destroy']);
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::get('/bookmarks', [BookmarkController::class, 'index'])->name('bookmarks.index');
+});
 
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
-Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update')->middleware('auth');
